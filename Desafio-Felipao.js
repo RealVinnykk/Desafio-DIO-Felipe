c141615da804fc62f1512c1a18c0324667397903
RealVinnykk <viniciusrafael2435@gmail.com>
2023-10-11 22:44:48 -0300

    Ultimo Patch Desafio

--- Desafio-Felipao.js
+++ Desafio-Felipao.js
@@ -1,42 +1,41 @@
-let quantidadeXP = Math.random() * 10000;
+let quantidadeXP = 100050
 let nomeHeroi = "RealVinnykk"
 
 if (quantidadeXP < 1000){
-    console.log("Seu heroi está no nivel de Ferro")
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Ferro")
 }
 
 if (quantidadeXP >= 1001, quantidadeXP <= 2000  ){
-    console.log("Seu heroi está no nivel de Bronze")
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Bronze")
 }
 
-
 if (quantidadeXP >= 2001, quantidadeXP <= 5000)  {
-    console.log("Seu heroi está no nivel de Prata")
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Prata")
 }
 
 
 if (quantidadeXP >= 6001, quantidadeXP <= 7000)  {
-    console.log("Seu heroi está no nivel de Ouro")
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Ouro")
 }
 
 
 if (quantidadeXP >= 7001, quantidadeXP <= 8000  ){
-    console.log("Seu heroi está no nivel de Platina")
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Platina")
+}
+
+
+if (quantidadeXP >= 8001, quantidadeXP <= 9000  ){
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Ascendente")
 }
 
 
 if (quantidadeXP >= 9001, quantidadeXP <= 10000  ){
-    console.log("Seu heroi está no nivel de Ascendente")
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Imortal")
 }
 
 
-if (quantidadeXP >= 1001, quantidadeXP <= 2000  ){
-    console.log("Seu heroi está no nivel de Imortal")
-}
-
-
-if (quantidadeXP >= 10000 ){
-    console.log("Seu heroi está no nivel de Radiante")
+if (quantidadeXP >= 10001 ){
+    console.log("o heroi : " + nomeHeroi + " está no nivel de Radiante")
 }
 
 
