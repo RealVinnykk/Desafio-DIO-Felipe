3a5ccae696d93123b91ee1778b91819a0f903263
RealVinnykk <viniciusrafael2435@gmail.com>
2023-10-11 22:39:35 -0300

    Desafio

---
+++ Desafio-Felipao.js
@@ -0,0 +1,43 @@
+let quantidadeXP = Math.random() * 10000;
+let nomeHeroi = "RealVinnykk"
+
+if (quantidadeXP < 1000){
+    console.log("Seu heroi está no nivel de Ferro")
+}
+
+if (quantidadeXP >= 1001, quantidadeXP <= 2000  ){
+    console.log("Seu heroi está no nivel de Bronze")
+}
+
+
+if (quantidadeXP >= 2001, quantidadeXP <= 5000)  {
+    console.log("Seu heroi está no nivel de Prata")
+}
+
+
+if (quantidadeXP >= 6001, quantidadeXP <= 7000)  {
+    console.log("Seu heroi está no nivel de Ouro")
+}
+
+
+if (quantidadeXP >= 7001, quantidadeXP <= 8000  ){
+    console.log("Seu heroi está no nivel de Platina")
+}
+
+
+if (quantidadeXP >= 9001, quantidadeXP <= 10000  ){
+    console.log("Seu heroi está no nivel de Ascendente")
+}
+
+
+if (quantidadeXP >= 1001, quantidadeXP <= 2000  ){
+    console.log("Seu heroi está no nivel de Imortal")
+}
+
+
+if (quantidadeXP >= 10000 ){
+    console.log("Seu heroi está no nivel de Radiante")
+}
+
+
+
